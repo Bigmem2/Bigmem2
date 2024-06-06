54ae9b48867d37c5110568b9ea99aa9a61ad8ec8
Michael Kleinsasser <mkleinsa@umich.edu>
2024-06-06 12:50:54 -0400

    all

--- tests/test_initialize.R
+++ tests/test_initialize.R
@@ -2,4 +2,7 @@
 library(MassiveTable)
 
 # create an MTTable object and get a pointer
-# mttable =
+mttable = MassiveTable::create_MTTable()
+
+# call the r_initiate method on the object being pointed at
+MassiveTable::r_initiateMTTable(mttable, "exdata.csv")
