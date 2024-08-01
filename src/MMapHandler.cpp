ed7ee22fbf5fdc934a79e6714646e2199b5aaaed
Michael Kleinsasser <mkleinsa@umich.edu>
2024-08-01 16:36:45 -0400

    m

--- src/MMapHandler.cpp
+++ src/MMapHandler.cpp
@@ -1,1 +1,57 @@
+#include <Rcpp.h>
+#include <sys/mman.h>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include "FileHandler.h"
+#include "MMapHandler.h"
+
+MMapHandler::MMapHandler(const std::string& filename)
+  : file(filename), fileData_ptr(nullptr) {
+
+  fileData_ptr = mmap(nullptr, file.get_fileSize(), PROT_READ, MAP_PRIVATE,
+                      file.get_fd(), 0);
+}
+
+MMapHandler::~MMapHandler() {
+
+  if (fileData_ptr && fileData_ptr != MAP_FAILED) {
+
+    munmap(fileData_ptr, file.get_fileSize());
+  }
+
+}
+
+
+const char* MMapHandler::get_fileData_ptr() const {
+
+  return static_cast<const char*>(fileData_ptr);
+}
+
+// read raw data function
+// read binary data function
+
+// test examples in cpp
+int main() {
+
+  try {
+
+    const std::string file_name = "exdata.csv";
+
+    MMapHandler mmap_handler(file_name);
+
+
+
+  } catch(const std::exception& e) {
+
+    std::cerr << e.what() << std::endl;
+  }
+}
+
+// compile on the fly for testing just this component
+// clang++ -arch arm64 -std=gnu++17 -I"/Library/Frameworks/R.framework/Resources/include" -DNDEBUG -I'/Library/Frameworks/R.framework/Versions/4.4-arm64/Resources/library/Rcpp/include' -I/opt/R/arm64/include -I/opt/homebrew/opt/llvm/include -Xclang -fopenmp -I./src -fPIC -falign-functions=64 -Wall -g -O2 -o MMapHandlerTest src/MMapHandler.cpp -L/Library/Frameworks/R.framework/Resources/lib -lR
+
+// run: ./MMapHandlerTest
+// remove: rm MMapHandlerTest
+
 
--- src/MMapHandler.h
+++ src/MMapHandler.h
@@ -2,23 +2,30 @@
 #define MMAPHANDLER_H
 
 #include <Rcpp.h>
+#include <string>
+#include <sys/types.h>
 #include "FileHandler.h"
 
 class MMapHandler {
 
 
 public:
+
+MMapHandler(const std::string& filename);
+
+~MMapHandler();
+
+
+const char* get_fileData_ptr() const;
+
 // read raw data function
-
-
 // read binary data function
 
 
 
 private:
   FileHandler file;
-
-
+  void* fileData_ptr;
 
 };
 
