6a69a6b0dab6020d0b7cf6dee7c72cd74ce2dc30
Michael Kleinsasser <mkleinsa@umich.edu>
2024-08-06 15:22:13 -0400

    m

--- src/TransposeDataHandler.cpp
+++ src/TransposeDataHandler.cpp
@@ -1,1 +1,8 @@
+#include <Rcpp.h>
+#include "TransposeDataHandler.h"
 
+TransposeDataHandler::TransposeDataHandler(const std::string& filename, off_t chunk_size)
+  : read_data(filename, chunk_size) {
+
+
+}
